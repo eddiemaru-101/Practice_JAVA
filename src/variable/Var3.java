8a9d756c2e20388119fa44eb8885985e4a0dd979
eddie <eddiemaru101@gmail.com>
2025-08-05 21:14:24 +0900

    ADD Variable CODE

---
+++ src/variable/Var3.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a;   //변수선언
+        a = 10;  //변수 초기화
+
+        System.out.println(a);
+        a = 50;   //변수값 변경 a(10 -> 50)
+        System.out.println(a);
+    }
+}
---
+++ src/variable/Var5.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        // 1. 변수 선언, 초기화 각각 따로
+        int a;
+        a = 10;
+
+        int b = 100; // 2. 변수 선언과 초기화 한번에
+        int c = 3, d = 6;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
